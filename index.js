ca1181a05409c998b62e3c4fe489bd7279adb91a
jaredworkman21 <jaredrussellworkman@gmail.com>
2021-07-01 17:50:17 -0600

    adding cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const jsonData = require("./data.json");
+var cors = require('cors')
+
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send(jsonData);
