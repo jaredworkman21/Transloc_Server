9eceb3eb9b8116cb6fcafa71de02951951d822a7
jaredworkman21 <jaredrussellworkman@gmail.com>
2021-07-01 17:23:35 -0600

    fixing heroku not loading

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get("/", (req, res) => {
     res.send("Location Data");
 })
 
-app.listen(2020, () => {
-    console.log("listening on 2020")
+app.listen(process.env.PORT || 2020, () => {
+    console.log("listening")
 })
